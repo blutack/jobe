bfb2fe582e238711fc4dad2168bbe4cee5baca1d
Gareth R <blutack@gmail.com>
2015-02-27 11:36:17 +0000

    Add docstrings for most functions, remove unused config variables

--- jobe.py
+++ jobe.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python3
 
+# JOBE - the git batch job runner
+
+# This file is designed to run as a git post-receive hook in a bare repository
+# The accompanying makefile can be used to setup a repository.
+
+# Alternatively, create a bare git repository, add this file as post-receive
+# to the hooks directory, clone the repo temporarily, touch a temporary file and push.
+# JOBE will detect the invalid state and set up master ready for use.
+
 import sys, tempfile, os, configparser, time
 from datetime import timedelta, datetime
 import subprocess as sp
@@ -24,19 +33,27 @@ verbose = no
 '''
 
 class Worker:
+    """Class used to detach and execute job processes."""
+    
     def __init__(self, job_id):
         self.job_id = job_id
         
+    # Call this file again in a new process, detaching from original controlling process to allow git to return
+    # Called from the git owned process
     def spawn(self, detach):
+        """Creates a new python process for each job, passing the job id as argument."""
+
         proc = sp.Popen(["python3", os.path.realpath(__file__), self.job_id], stdout=sp.PIPE, stderr=sp.PIPE, stdin=sp.PIPE)
         
         if not detach:
+            # TODO: Python 3.3 and newer have a timeout keyword option for communicate, which would prevent deadlocking
             stdout, stderr = proc.communicate()
             
             p.debug(stdout)
             p.debug(stderr)      
     
     def execute(self, config, repo):
+        """Executes a job from the passed configuration object, blocks until complete."""
         if config.wait > 0:
             time.sleep(config.wait)
         proc = sp.Popen(config.command, stdout=repo.open_file("stdout.log"), 
@@ -45,7 +62,9 @@ class Worker:
         with repo.open_file("exitcode.log") as exit_file:
             print(proc.returncode, file=exit_file)
     
+    # Called from the detached process
     def run(self):
+        """Checks out a branch containing a job and executes the command included."""
         with Repo(repo_dir) as repo:
             repo.clone()
             repo.checkout(self.job_id)
@@ -56,7 +75,8 @@ class Worker:
             repo.push()
     
 class Printer:
-    verbose = False
+    """Utility class for pretty printing."""
+    verbose = False # Controls debug output
     
     info = lambda _, output: print('\033[94m>>> ' + output + '\033[0m')
     ok = lambda _, output: print('\033[92m>>> ' + output + '\033[0m')
@@ -68,10 +88,16 @@ class Printer:
         if self.verbose:
             self.warn(str(output)) 
 
-class Repo:    
+# Ideally this would be done using Dulwich or something, but I aimed to only depend on the standard library.
+class Repo:
+    """A very basic wrapper around various git operations."""
+    
     def __init__(self, location):
+        """Specifies a git remote url for the repo to be checked out."""
         self.location = location
     
+    # Context managers to allow with Repo()... use.
+    # TemporaryDirectory is only available since Python 3.2
     def __enter__(self):
         self.__tmp_dir = tempfile.TemporaryDirectory()
         self.work_dir = self.__tmp_dir.name
@@ -81,13 +107,14 @@ class Repo:
         self.__tmp_dir.cleanup()
         
     def reset(self):
+        """Remove contents of master and replace with sample configuration."""
         p.debug("Rolling back master")
         self.checkout("master")
 
         f = self.open_file("jobe.ini", 'a+')
         
         if f.read() != sample_config or len(os.listdir(self.work_dir)) > 2:
-                f.close()
+                f.close() # Make sure we close the file handle before removing the file
                 self.git("rm -f *")
                 
                 with self.open_file("jobe.ini", 'w') as new_sample:
@@ -100,29 +127,38 @@ class Repo:
         return open(os.path.join(self.work_dir, file_name), mode)
         
     def clone(self):
+        """Clone the repository to a temporary working directory."""
         self.git("clone " + self.location + " " + self.work_dir)
     
     def branch(self, branch_name):
+        """Create a new git branch."""
         self.git("checkout -b " + branch_name)
         
     def checkout(self, branch_name):
+        """Checkout an existing branch."""
         self.git("checkout " + branch_name)
         
     def add_all(self):
+        """Add all new files to the branch."""
         self.git("add -A")
         
     def add(self, file_name):
+        """Add a single file to the branch."""
         self.git("add " + file_name)
     
     def commit(self, message):
+        """Commit with the given message."""
         self.git("commit -a -m '" + message + "'")
         
     def push(self, branch = "--all"):
+        """Push branches"""
         print(self.git("push " + branch))
         
     def short_hash(self):
+        """Return the git short hash for the current head."""
         return self.git("rev-parse --short HEAD")
-        
+    
+    # TODO: Don't use shell=True and fix the nasty path posixisms.
     def git(self, command):
         proc = sp.Popen("git --git-dir=" + self.work_dir + "/.git" + " " + command, shell=True, cwd = self.work_dir, 
              stdout=sp.PIPE, stderr=sp.PIPE, universal_newlines=True)
@@ -142,13 +178,12 @@ class Config:
         self.config_file.read(os.path.join(repo.work_dir, self.filename))
         
         if not 'jobe' in self.config_file:
+            # Short circuit and return a config object with valid = False
             return
             
         self.command = self.config_file['jobe'].get('command')
         
         self.job_id = self.config_file['jobe'].get('name', 'job') + '-' + repo.short_hash()
-        
-        self.timeout = self.config_file['jobe'].get('timeout', 10)
         
         self.detach = self.config_file['jobe'].getboolean('detach', False)
         p.verbose = self.config_file['jobe'].getboolean('verbose', False)
@@ -156,22 +191,27 @@ class Config:
         self.run_at = self.config_file['jobe'].get('run_at')
         self.run_at = datetime.strptime(self.run_at, "%Y-%m-%dT%H:%M:%S.%f")
         
-        self.wait = (self.run_at - datetime.now()).total_seconds()
+        self.wait = (self.run_at - datetime.now()).total_seconds() # Wait time in floating point seconds until execution.
         self.valid = True
 
 def branch_only_master():
+    """Utility function to read stdin and determine whether only head is being pushed to."""
     lines = sys.stdin.readlines()
     p.debug(lines)
     refs = lines[0].split(' ')
     return len(lines) == 1 and "refs/heads/master" in refs[2]
-    
+
+# This isn't strictly needed as a git hook is unlikely to be used as a library
 if __name__ == "__main__":
     p = Printer()
     
+    # Find the local path of the jobe repository (the repository the git hook is running from)
     repo_dir = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))
     
+    # TODO: check properly that any arguments are valid job ids.
+    # Currently, if called with no arguments we assume it is the git process.
+    # If called with a job argument, assume we are the second, detached process.
     if len(sys.argv) <= 1:
-        # Standard push
         p.banner()
         if branch_only_master():
             with Repo(repo_dir) as repo:
@@ -196,9 +236,10 @@ if __name__ == "__main__":
                 p.ok("To retrieve your job, do:")
                 p.ok("git pull --all && git checkout " + config.job_id)
         else:
-            p.warn("Push only to master to create jobs")
+            p.warn("To create a job, push only to the master branch.")
 
     else:
+        # We are the detached process, so create a worker and run the process blocking
         w = Worker(sys.argv[1])
         w.run()
         
